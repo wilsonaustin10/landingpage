b9936f215e6c0569886a57d25d511d30dab18dbd
Austin Wilson <wilson.austin10@gmail.com>
2025-03-26 21:56:22 -0500

    refactor: Replace Allied House Buyers with JR Home Buyer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,11 +31,11 @@ export default function Footer() {
                 <span>(929)-499-2303</span>
               </a>
               <a 
-                href="mailto:info@FastCashForHomesJR.com"
+                href="mailto:info@jrhomebuyer.com"
                 className="flex items-center space-x-2 text-white/80 hover:text-white transition-colors"
               >
                 <Mail className="h-4 w-4" />
-                <span>info@alliedhousebuyers.com</span>
+                <span>info@jrhomebuyer.com</span>
               </a>
             </div>
           </div>
